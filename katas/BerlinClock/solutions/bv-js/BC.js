ba62d60848c29e8d3e24ef45fbdab94e7bebb5a2
Bojan <bojanvidaic@hotmail.com>
2022-12-27 11:08:10 +0100

    berlinclock code optimization

--- katas/BerlinClock/solutions/bv-js/BC.js
+++ katas/BerlinClock/solutions/bv-js/BC.js
@@ -1,65 +1,40 @@
 $( document ).ready(function() {
+    // helper function - calculate time
+    function calculateTime(time, step = 5) {
+        return [
+            Math.floor(time/step),
+            time % step
+        ];
+    }
+    // helper function - print time
+    function printTime(selector, count) {
+        let allItems = $(selector);
+        allItems.slice(0, count).each(function(index, element) {
+            $(element).addClass('opacity-1');
+        });
+    }
+    // main function
     function generateBerlinClock() {
         var today = new Date();
-        var hours = today.getHours();
-        var minutes = today.getMinutes();
-        var seconds = today.getSeconds();
-    
-        var ho5 = 0;
-        while (hours >= 5) {
-            hours -= 5;
-            ho5++;
-        }
-    
-        var ho1 = 0;
-        while (hours > 0) {
-            hours -= 1;
-            ho1++;
-        }
-    
-        var m5 = 0;
-        while (minutes >= 5) {
-            minutes -= 5;
-            m5++;
-        }
-    
-        var m1 = 0;
-        while (minutes > 0) {
-            minutes -= 1;
-            m1++;
-        }
-    
-console.log(ho5, ho1, m5, m1);
+        // calculate hours
+        calculatedHours = calculateTime(today.getHours());
+        // calculate minutes
+        calculatedMinutes = calculateTime(today.getMinutes());
         // reset all
         $('div').removeClass('opacity-1');
-        
-        var m5divs = $('.m5 div');
-        m5divs.slice(0, m5).each(function(index, element) {
-            $(element).addClass('opacity-1');
-        });
-    
-        var m1divs = $('.m1 div');
-        m1divs.slice(0, m1).each(function(index, element) {
-            $(element).addClass('opacity-1');
-        });
-    
-        var ho5divs = $('.ho5 div');
-        ho5divs.slice(0, ho5).each(function(index, element) {
-            $(element).addClass('opacity-1');
-        });
-    
-        var ho1divs = $('.ho1 div');
-        ho1divs.slice(0, ho1).each(function(index, element) {
-            $(element).addClass('opacity-1');
-        });
-    
-        $('.sec div').removeClass('opacity-1');
-        if (seconds % 2) {
+        // print hours
+        printTime('.ho5 div', calculatedHours[0]);
+        printTime('.ho1 div', calculatedHours[1]);
+        // print minutes
+        printTime('.m5 div', calculatedMinutes[0]);
+        printTime('.m1 div', calculatedMinutes[1]);
+        // print seconds
+        if (today.getSeconds() % 2) {
             $('.sec div').addClass('opacity-1');
         }
     }
-    generateBerlinClock();
 
+    generateBerlinClock();
 
     setInterval(function() {
         generateBerlinClock();
