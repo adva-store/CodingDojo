fd22dbfdc4ac55b307f79149c7c2b13e87694e2d
baxa68486 <34898824+baxa68486@users.noreply.github.com>
2023-01-15 22:34:43 +0100

    restructure the folders

--- katas/Palindrom/src/main/Main.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Main {
-    public static void main(String[] args) {
-        var res = new Palindrome().isPalindrome("amma");
-        System.out.println(res);
-        System.out.println("Hello world!");
-    }
-}
