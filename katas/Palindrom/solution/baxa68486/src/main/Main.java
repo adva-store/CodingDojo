2febe319b8176447f9eae8832841177cf3a5db9f
baxa68486 <34898824+baxa68486@users.noreply.github.com>
2023-01-15 22:36:03 +0100

    added files

---
+++ katas/Palindrom/solution/baxa68486/src/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        var res = new Palindrome().isPalindrome("amma");
+        System.out.println(res);
+        System.out.println("Hello world!");
+    }
+}
