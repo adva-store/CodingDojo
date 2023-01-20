0f09becaee98f103bd3aa188b48b86bf86b55727
Janek Beicher <janek.beicher@alley.de>
2023-01-20 12:54:30 +0100

    Update docs

--- katas/LangtonAnt/solutions/jbchr/apps/api/src/app/langton-ant.service.ts
+++ katas/LangtonAnt/solutions/jbchr/apps/api/src/app/langton-ant.service.ts
@@ -16,19 +16,19 @@ export class LangtonAntService {
    *
    * The algorithm operates on an array of turns. Each turn is represented as an one dimensional array that represents the grid.
    * Each field consists of the state as second char (s/w for black/white) and the ant's direction (n,o,s,w, space for north, east, south, west, no ant) as first char.
-   * The algorithm's data structure is close to the outputs data structure, so in the end only the spaces are removed and the
-   * fields are joined with a comma and the turns with a semilcolon.
+   * The algorithm's data structure is close to the outputs data structure.
    *
    * Note: The grid is infinite. If the ant moves out of boundary in the east direction it will return in the west
    *
    * @example
    * computeGame(2, 0, 0, 'n', 2)
    * Returns
-   * nw,w,w,w;
-   * s,ow,w,w
+   * [
+   *   [nw,w,w,w]
+   *   [s,ow,w,w]
+   * ]
    *
-   * @returns the game as string. Each turn is seperated with a semilkolon, each field in a turn is seperated by comma.
-   * Each filed has the state (s/w for black/white) and optionaly as prefix the ant's direction (n,o,s,w for north, east, south, west)
+   * @returns the game as an array of turns. Each turn consists of all fields of the board.
    */
   computeGame(
     boardLength: number,
