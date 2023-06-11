317dcd90bc97fb18abbb21172c0e9b10882e41ee
Marcel Makowski <stereoify@gmail.com>
2023-06-11 23:44:34 +0200

    Initial commit - Strange Chessobard.

---
+++ katas/StrangeChessboard/solution/monoik/StrangeChessboard/StrangeChessboard/ChessboardCalculationUnitTest.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+
+// Class containing tests for the "Strange chessboard" areas calculation.
+namespace StrangeChessboard
+{
+	[TestClass]
+	public class ChessboardCalculationUnitTest
+	{
+		[TestMethod]
+		public void SimpleTest1()
+		{
+			int[] fieldsWidths = { 10, 3, 5 };
+			int[] fieldHeights = { 3, 8, 22 };
+
+			int whiteArea = 10 * 3 + 10 * 22 + 3 * 8 + 5 * 3 + 5 * 22;
+			int blackArea = 10 * 8 + 3 * 3 + 3 * 22 + 5 * 8;
+			RunChessboardCalculation(fieldsWidths, fieldHeights, whiteArea, blackArea);
+		}
+
+		private void RunChessboardCalculation(int[] fieldsWidths, int[] fieldHeights, int expectedWhiteArea, int expecterBlackArea)
+		{
+			StrangeChessboardCalculator.GetAreasOfStrangeChessboard(fieldsWidths, fieldHeights, out (int total_white_area, int total_black_area) areas);
+
+			Assert.AreEqual(expectedWhiteArea, areas.total_white_area);
+			Assert.AreEqual(expecterBlackArea, areas.total_black_area);
+
+			Assert.AreEqual(fieldHeights.Sum() * fieldsWidths.Sum(), areas.total_black_area + areas.total_white_area);
+		}
+
+		[TestMethod]
+		public void SimpleTest2()
+		{
+			// Test from the desctiption of the Kata.
+			int[] fieldsWidths = { 3, 1, 2, 7, 1 };
+			int[] fieldHeights = { 1, 8, 4, 5, 2 };
+
+			int whiteArea = 146;
+			int blackArea = 134;
+
+			RunChessboardCalculation(fieldsWidths, fieldHeights, whiteArea, blackArea);
+		}
+
+
+		[TestMethod]
+		public void Test3()
+		{
+			// All the fields have a size of 1. Even length/height of the chessboard. White and black are equal.
+			int[] fieldsWidths = Enumerable.Repeat(1, 3000).ToArray();
+			int[] fieldHeights = Enumerable.Repeat(1, 3000).ToArray();
+
+			int whiteArea = 4500000;
+			int blackArea = 4500000;
+
+			RunChessboardCalculation(fieldsWidths, fieldHeights, whiteArea, blackArea);
+		}
+
+		[TestMethod]
+		public void Test4()
+		{
+			// All the fields have a size of 1. Odd length/height of the chessboard. There is one more white field than black.
+			int[] fieldsWidths = Enumerable.Repeat(1, 3001).ToArray();
+			int[] fieldHeights = Enumerable.Repeat(1, 3001).ToArray();
+
+			int whiteArea = 4503001;
+			int blackArea = 4503000;
+
+			RunChessboardCalculation(fieldsWidths, fieldHeights, whiteArea, blackArea);
+		}
+
+
+	}
+}
---
+++ katas/StrangeChessboard/solution/monoik/StrangeChessboard/StrangeChessboard/StrangeChessboardCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StrangeChessboard
+{
+	internal class StrangeChessboardCalculator
+	{
+		/*
+		 * Calculates the total areas of white and black fields of the "Strange chessboard".
+		 */
+		internal static void GetAreasOfStrangeChessboard(int[] fieldWidths, int[] fieldHeights, out (int total_white_area, int total_black_area) areas)
+		{
+			List<int> areasList = new List<int>();
+
+			for (int i = 0; i < fieldWidths.Length; i++)
+			{
+				for (int j = 0; j < fieldHeights.Length; j++)
+				{
+					// Add the areas to a single big list.
+					areasList.Add(fieldWidths[i] * fieldHeights[j]);
+				}
+			}
+
+			// Odd areas are white (counting from 0). Even ones are black. We sum them up.
+			int whiteAreaCalculated = areasList.Where((item, index) => index % 2 == 0).Sum();
+			int blackAreaCalculated = areasList.Where((item, index) => index % 2 != 0).Sum();
+			areas = (whiteAreaCalculated, blackAreaCalculated);
+		}
+	}
+}
